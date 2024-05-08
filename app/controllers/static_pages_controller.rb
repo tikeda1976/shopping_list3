e876f216154d56f9af86372b116ef0be26995448
tikeda1976 <167026683+tikeda1976@users.noreply.github.com>
2024-05-08 12:26:20 +0000

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    # => app/views/static_pages/home.html.erb
   end
 
   def help
+    # => app/views/static_pages/help.html.erb
   end
 
   def about
+    # => app/views/static_pages/about.html.erb
+  end
+
+  def contact
+    # => app/views/static_pages/contacvt.html.erb
   end
 end
