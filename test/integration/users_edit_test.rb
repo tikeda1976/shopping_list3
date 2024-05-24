dbe0f19d0c61fb76a34aa6e18958042e1abe0ad1
tikeda1976 <167026683+tikeda1976@users.noreply.github.com>
2024-05-24 13:58:26 +0000

    Finish ch10 user edit, update, index, and destroy actions

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -21,7 +21,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert_equal session[:forwarding_url], edit_user_url(@user) #ここでないとエラーになる。
     log_in_as(@user)
+    assert_nil session[:forwarding_url] #ここ以降はnilになる。
     assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
