f37c2a24143f241d4c53a1b07a7c1d6e3e4b53a6
N4oto-T <h13.only57@gmail.com>
2024-12-30 21:52:14 +0900

    itemにカラム「quantity」を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_24_111225) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_30_125102) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_24_111225) do
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
 
   create_table "lists", force: :cascade do |t|
