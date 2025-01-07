1f77be1b5ebfe6e9e2de1a76cb67639d20f41a9a
a2419ms <a2419ms@aiit.ac.jp>
2025-01-07 10:14:32 +0000

    item削除のルーティング追加と、コントローラのアクション追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_24_102814) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_24_111225) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_24_102814) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -83,4 +89,3 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_24_102814) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "microposts", "users"
 end
-
