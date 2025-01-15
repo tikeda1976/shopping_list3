c1d2da4d8f0cdddce4e8093a079c941ff4935e08
田島裕崇 <a2423ht@aiit.ac.jp>
2025-01-15 21:56:45 +0900

    ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  root 'lists#index' # ホーム画面をリスト一覧に設定
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
@@ -16,9 +17,10 @@ Rails.application.routes.draw do
       get :detail, to: 'details#show' # 詳細画面
     end
     resources :items, only: %i[create destroy], controller: 'details'
-  end
+  end  
   resources :account_activations, only: [:edit]
-  resources :microposts, only: %i[create destroy]
-  resources :relationships, only: %i[create destroy]
+  resources :microposts,          only: %i[create destroy]
+  resources :relationships,       only: %i[create destroy]
+  resources :lists, only: %i[index create destroy]
   get '/microposts', to: 'static_pages#home'
 end
